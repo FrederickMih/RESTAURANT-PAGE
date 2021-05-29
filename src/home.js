bcf02786b3c4da41cc7083d32dc64e70b7ca9d77
FrederickMih <fredtimermih@gmail.com>
2021-05-29 16:20:04 +0100

    Modify pages

--- src/home.js
+++ src/home.js
@@ -3,20 +3,27 @@ const displayHome = () => {
   const homeTitle = document.createElement("h2");
   const homeNote = document.createElement("h5");
   const homeImg = document.createElement("img");
+  const homeImg1 = document.createElement("img");
+  const homeImg2 = document.createElement("img");
 
   homeTitle.innerText = "A Taste of Africa - Recipes";
-  homeNote.innerText = "Words are sweet but they can never replace food!"
+  homeNote.innerText = "Words are sweet but they can never replace food!";
 
   home.setAttribute("class", "main");
   home.setAttribute("id", "homePage");
   homeTitle.setAttribute("class", "home-title");
   homeNote.setAttribute("class", "home-note");
   homeImg.setAttribute("class", "home-img");
-  homeImg.setAttribute("src", "../assets/images/yellow-soup.jpg");
+  
+
+  homeImg.setAttribute("src", "../assets/images/lauch.jpg");
+  
 
   home.appendChild(homeTitle);
   home.appendChild(homeNote);
   home.appendChild(homeImg);
+  home.appendChild(homeImg1);
+  home.appendChild(homeImg2);
 
   return home;
 };
--- src/menus.js
+++ src/menus.js
@@ -7,14 +7,20 @@ const displayMenu = () => {
   const meal1 = document.createElement("a");
   const meal2 = document.createElement("a");
   const meal3 = document.createElement("a");
+  const meal4 = document.createElement("a");
+  const meal5 = document.createElement("a");
 
   const imgMeal1 = document.createElement("img");
   const imgMeal2 = document.createElement("img");
   const imgMeal3 = document.createElement("img");
+  const imgMeal4 = document.createElement("img");
+  const imgMeal5 = document.createElement("img");
 
   const parMeal1 = document.createElement("p");
   const parMeal2 = document.createElement("p");
   const parMeal3 = document.createElement("p");
+  const parMeal4 = document.createElement("p");
+  const parMeal5 = document.createElement("p");
 
   
   meals.setAttribute("class", "meals");
@@ -24,20 +30,32 @@ const displayMenu = () => {
   meal2.setAttribute("href", "#");
   meal3.setAttribute("class", "meal");
   meal3.setAttribute("href", "#");
+  meal4.setAttribute("class", "meal");
+  meal4.setAttribute("href", "#");
+  meal5.setAttribute("class", "meal");
+  meal5.setAttribute("href", "#");
 
   imgMeal1.setAttribute("class", "img-meal");
-  imgMeal1.setAttribute("src", "../assets/images/eru.jpg");
+  imgMeal1.setAttribute("src", "../assets/images/Ekwang.jpg");
   imgMeal2.setAttribute("class", "img-meal");
-  imgMeal2.setAttribute("src", "../assets/images/eru.jpg");
+  imgMeal2.setAttribute("src", "../assets/images/yellow-soup.jpg");
   imgMeal3.setAttribute("class", "img-meal");
-  imgMeal3.setAttribute("src", "../assets/images/eru.jpg");
+  imgMeal3.setAttribute("src", "../assets/images/Egusi-Pudding-Recipe.jpg");
+  imgMeal4.setAttribute("class", "img-meal");
+  imgMeal4.setAttribute("src", "../assets/images/eru.jpg");
+  imgMeal5.setAttribute("class", "img-meal");
+  imgMeal5.setAttribute("src", "../assets/images/eru.jpg");
 
   parMeal1.setAttribute("class", "par-meal");
-  parMeal1.innerText = "Dish One";
+  parMeal1.innerText = "Dish One(Ekwang)";
   parMeal2.setAttribute("class", "par-meal");
-  parMeal2.innerText = "Dish Two";
+  parMeal2.innerText = "Dish Two(Yellow Soup)";
   parMeal3.setAttribute("class", "par-meal");
-  parMeal3.innerText = "Dish Three";
+  parMeal3.innerText = "Dish Three (Egusi Pudding)";
+  parMeal4.setAttribute("class", "par-meal");
+  parMeal4.innerText = "Dish Four";
+  parMeal5.setAttribute("class", "par-meal");
+  parMeal5.innerText = "Dish Five";
 
   homeNote.innerText = "Find The True Taste Here!";
 
@@ -61,6 +79,11 @@ const displayMenu = () => {
   meal2.appendChild(parMeal2);
   meal3.appendChild(imgMeal3);
   meal3.appendChild(parMeal3);
+  meal4.appendChild(imgMeal4);
+  meal4.appendChild(parMeal4);
+  meal5.appendChild(imgMeal5);
+  meal5.appendChild(parMeal5);
+  
   return menu;
 };
 
