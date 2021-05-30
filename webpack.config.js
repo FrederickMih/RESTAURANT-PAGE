bd1960fd6a1ca8a58bfab5ac83db365604553e2a
FrederickMih <fredtimermih@gmail.com>
2021-05-30 07:46:03 +0100

    Stylelint and eslint fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.scss$/i,
         use: [
-         "style-loader", // 3. Inject styles into DOM
-         "css-loader", // 2. Turns css into commonjs
-         "sass-loader" // 1. Turns sass into css
+          'style-loader', // 3. Inject styles into DOM
+          'css-loader', // 2. Turns css into commonjs
+          'sass-loader', // 1. Turns sass into css
         ],
       },
     ],
